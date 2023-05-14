1b277fe4cc04be19565b61ee37a914402335aa19
Parame Srisomsap <s6304062616170@email.kmutnb.ac.th>
2023-05-14 22:30:22 +0700

    hehehehe

--- frontend/src/components/PatientData.tsx
+++ frontend/src/components/PatientData.tsx
@@ -2,20 +2,43 @@
 
 import React,{ useEffect, useState } from 'react';
 import axios from 'axios';
-import {Form,Button} from 'react-bootstrap';
+import { Table} from 'react-bootstrap';
 
 
 const PatientData:React.FC = ()=>{
 
 
-    useEffect(() => {
-        axios.get('')
-    },[])
     const [PatientData,SetPatientData] = useState([]);
 
+    
+    useEffect(() => {
+        axios.get('http://localhost:5555/getqueuedata')
+        .then(res => {
+            SetPatientData(res.data)
+        })
+        .catch(err=> alert(err))
+    },[])
+
     return(
-       <div>
-           
+       <div className='table-patient'>
+           <Table className='table' style={{width:1450,backgroundColor:'white',borderRadius:5}}>
+                <thead>
+                      <th>ชื่อผู้ป่วย</th>
+                      <th>อายุ</th>
+                      <th>เพศ</th>
+                      <th>ที่อยู่</th>
+                      <th>รหัสประจำตัว</th>
+                      <th>วันที่เข้าตรวจ</th>
+                    
+                      
+                </thead>
+
+                <tbody>
+                    <tr>
+                        <td>1</td>
+                    </tr>
+                </tbody>
+           </Table>
        </div>
     )
 }

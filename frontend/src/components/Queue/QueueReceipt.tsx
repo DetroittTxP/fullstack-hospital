ef3979a838c623431ef52e952962312f587db8c3
Parame Srisomsap <109692462+DetroittTxP@users.noreply.github.com>
2023-05-19 16:08:31 +0700

    hehehehehe

--- frontend/src/components/Queue/QueueReceipt.tsx
+++ frontend/src/components/Queue/QueueReceipt.tsx
@@ -1,76 +1,95 @@
-import React,{useEffect} from 'react';
+import React, { useEffect } from 'react';
 import {
-    MDBCard,
-    MDBCardBody,
-    MDBCol,
-    MDBContainer,
-    MDBRow,
-    MDBTypography,
-  } from "mdb-react-ui-kit";
+  MDBCard,
+  MDBCardBody,
+  MDBCol,
+  MDBContainer,
+  MDBRow,
+  MDBTypography,
+} from "mdb-react-ui-kit";
 import { ReceiptQueueProps } from '../../type';
 
-const QueueRecceipt:React.FC<ReceiptQueueProps> = ({queuedata}) =>{  
+const QueueRecceipt: React.FC<ReceiptQueueProps> = ({ queuedata }) => {
 
-     
 
-    return(
-        <MDBContainer className="py-5">
-      <MDBCard>
-        <MDBCardBody className="mx-4">
-          <MDBContainer>
-            <p className="my-5 text-center" style={{ fontSize: "30px" }}>
-              จองการนัดตรวจสำเร็จเเล้ว
-            </p>
-            <MDBRow>
-              <MDBTypography listUnStyled>
-                <li className="text-black">ชื่อ-สกุล: <span style={{marginLeft:10}}>{queuedata.name}</span></li>
-               
-                <li className="text-black mt-1">เข้าตรวจวันที่: <span style={{marginLeft:10}}>{queuedata.date}</span></li>
-              </MDBTypography>
-              <hr />
-              <MDBCol xl="10">
-                <p>Pro Package</p>
-              </MDBCol>
-              <MDBCol xl="2">
-                <p className="float-end">$199.00</p>
-              </MDBCol>
-              <hr />
-            </MDBRow>
-            <MDBRow>
-              <MDBCol xl="10">
-                <p>Consulting</p>
-              </MDBCol>
-              <MDBCol xl="2">
-                <p className="float-end">$100.00</p>
-              </MDBCol>
-              <hr />
-            </MDBRow>
-            <MDBRow>
-              <MDBCol xl="10">
-                <p>Support</p>
-              </MDBCol>
-              <MDBCol xl="2">
-                <p className="float-end">$10.00</p>
-              </MDBCol>
-              <hr style={{ border: "2px solid black" }} />
-            </MDBRow>
-            <MDBRow className="text-black">
-              <MDBCol xl="12">
-                <p className="float-end fw-bold">Total: $10.00</p>
-              </MDBCol>
-              <hr style={{ border: "2px solid black" }} />
-            </MDBRow>
-            <div className="text-center" style={{ marginTop: "90px" }}>
-              <a>
-                <u className="text-info">View in browser</u>
-              </a>
-              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. </p>
-            </div>
-          </MDBContainer>
-        </MDBCardBody>
-      </MDBCard>
-    </MDBContainer>
-    )
+
+  return (
+    <>
+      <br />zz
+      <MDBContainer className="py-5" >
+        <MDBCard>
+          <MDBCardBody className="mx-4">
+            <MDBContainer>
+              <p className="my-5 text-center" style={{ fontSize: "30px" }}>
+                จองการนัดตรวจสำเร็จเเล้ว
+              </p>
+              <MDBRow>
+                <MDBTypography listUnStyled>
+                  <li className="text-black">ชื่อผู้ใช้ <span style={{ marginLeft: 10 }}>{queuedata.creator}</span></li>
+
+                  <li className="text-black mt-1">ที่อยู่ <span style={{ marginLeft: 10 }}>{queuedata.address}</span></li>
+                </MDBTypography>
+                <hr />
+                <MDBCol xl="10">
+                  <p>ชื่อผู้ป่วย:</p>
+                </MDBCol>
+                <MDBCol xl="2">
+                  <p className="float-end">{queuedata.name}</p>
+                </MDBCol>
+                <hr />
+              </MDBRow>
+              <MDBRow>
+                <MDBCol xl="10">
+                  <p>อายุ</p>
+                </MDBCol>
+                <MDBCol xl="2">
+                  <p className="float-end">{queuedata.age} ปี</p>
+                </MDBCol>
+                <hr />
+              </MDBRow>
+
+              <MDBRow>
+                <MDBCol xl="10">
+                  <p>เพศ</p>
+                </MDBCol>
+                <MDBCol xl="2">
+                  <p className="float-end">{queuedata.gender}</p>
+                </MDBCol>
+                <hr />
+              </MDBRow>
+              <MDBRow>
+                <MDBCol xl="10">
+                  <p>เลขประจำตัวประชาชน</p>
+                </MDBCol>
+                <MDBCol xl="2">
+                  <p className="float-end">{queuedata.id_card}</p>
+                </MDBCol>
+                <hr style={{ border: "2px solid black" }} />
+              </MDBRow>
+              <MDBRow className="text-black">
+                <MDBCol xl='1='>
+                    <p className='fw-bold'>วันที่เข้าตรวจ</p>
+                </MDBCol>
+                <MDBCol xl="12">
+                  <p className="float-end fw-bold">{queuedata.date}</p>
+                </MDBCol>
+                <hr style={{ border: "2px solid black" }} />
+              </MDBRow>
+
+
+              {/* <div className="text-center" style={{ marginTop: "90px" }}>
+                <a>
+                  <u className="text-info">View in browser</u>
+                </a>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. </p>
+              </div> */}
+            </MDBContainer>
+          </MDBCardBody>
+        </MDBCard>
+      </MDBContainer>
+    </>
+
+  )
 }
 
 export default QueueRecceipt;

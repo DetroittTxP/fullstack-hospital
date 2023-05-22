31ef3b29d4d89f7bdb048860ffefacaff575692b
Parame Srisomsap <109692462+DetroittTxP@users.noreply.github.com>
2023-05-22 09:35:15 +0700

    submited

--- frontend/src/components/Queue/Callqueue.tsx
+++ frontend/src/components/Queue/Callqueue.tsx
@@ -1,10 +1,17 @@
-import React from 'react';
+import {useState,FC} from 'react';
+import {Button,Card} from 'react-bootstrap'
+import { QueuePatient } from '../../type';
 
+const Callqueue:FC = () => {
 
-const Callqueue:React.FC = () => {
+    const [patientqueue,Setpatientqueue] = useState([1,2,3,4,5,6,7,8,9,10])
+
+    
+
     return(
-        <div>
-             test
+        <div className='callqueue-patient'>
+             <Button onClick={()=>{Setpatientqueue((prev) => prev.slice(1))}}>Call 1 ST</Button>
+             <p>{patientqueue[0]}</p>
         </div>
     )
 }

8771fa9a9786cb60b9d6be44826df47558cd1cfa
Parame Srisomsap <109692462+DetroittTxP@users.noreply.github.com>
2023-05-23 13:22:12 +0700

    heeeeeeeeeeeeee

--- frontend/src/components/Queue/Callqueue.tsx
+++ frontend/src/components/Queue/Callqueue.tsx
@@ -1,18 +1,73 @@
-import {useState,FC} from 'react';
-import {Button,Card} from 'react-bootstrap'
+import { useState, FC, useEffect } from 'react';
+import { Button, Card } from 'react-bootstrap'
 import { QueuePatient } from '../../type';
+import axios from 'axios';
 
-const Callqueue:FC = () => {
+const Callqueue: FC = () => {
 
-    const [patientqueue,Setpatientqueue] = useState([1,2,3,4,5,6,7,8,9,10])
+    const LoggedInUsername = localStorage.getItem('username')?.replace(/^"(.*)"$/, '$1')
+    const [patientqueue, Setpatientqueue] = useState<QueuePatient[]>([{
+        id: 0,
+        name: '',
+        age: '',
+        gender: '',
+        address: '',
+        id_card: '',
+        date: '',
+        creator: LoggedInUsername || undefined
+    }]);
+    const [runqueue,Setrunqueue] = useState<number>(1);
 
-    
 
-    return(
+    useEffect(() => {
+        axios.get(`http://localhost:5555/getqueuedata?LoggedInUsername=${LoggedInUsername}`)
+        .then(res => {
+            Setpatientqueue(res.data)
+        })
+        .catch(err=> alert(err))          
+
+    },[])
+
+    const onCallnextQueue=(id:number)=>{
+        if(patientqueue.length === 1){
+            alert('No more queue');
+            return;
+        }
+        let newqueue = patientqueue.filter((e) => e.id !== id)
+        Setpatientqueue(newqueue)
+        Setrunqueue(runqueue+1);
+
+    }
+
+
+
+    return (
         <div className='callqueue-patient'>
-             <Button onClick={()=>{Setpatientqueue((prev) => prev.slice(1))}}>Call 1 ST</Button>
-             <p>{patientqueue[0]}</p>
+            <Button onClick={() => { Setpatientqueue((prev) => prev.slice(1)) }}>Call next queue</Button>
+            <div className='callqueue-card' >
+
+                <Card >
+                    <Card.Body>
+                        <Card.Title>คิวที่ {runqueue}</Card.Title>
+                        <Card.Subtitle className="mb-2 text-muted">วันที่ {patientqueue[0].date}</Card.Subtitle>
+                        <Card.Text>
+                          ชื่อ: {patientqueue[0].name}<br/>
+                          อายุ: {patientqueue[0].age}<br/>
+                          เพศ: {patientqueue[0].gender}<br/>
+                          ที่อยู่: {patientqueue[0].address}<br/>
+                          เลขประจำตัว: {patientqueue[0].id_card}
+                        </Card.Text>
+                        <Button onClick={()=>onCallnextQueue(patientqueue[0].id)} style={{marginLeft:20}}>คิวถัดไป</Button>
+                        
+                    </Card.Body>
+                </Card>
+
+            </div>
+
         </div>
+
+
+
     )
 }
 
